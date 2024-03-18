19d313bdccea0a4602ea796967b1052541bc4c21
Cody Weiss <weissc16@gmail.com>
2024-03-18 12:23:03 -0600

    commit 5
    basic tower functionality
    using Physics.OverlapSphere() we are storing which colliders are in range in an array.
    created targeting system for towers using arrays, lists and colliders.
    created cannonball using rigidbody system.
    created script to have tower shoot cannoball once per second.

---
+++ TowerDefense/Assets/Scripts/Towers/ProjectileTower.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileTower : MonoBehaviour
+{
+    private Tower thetower;
+
+    public GameObject projectile;
+    public Transform firePoint;
+    public float timeBetweenShots = 1f;
+    private float shotCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thetower = GetComponent<Tower>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0)
+        {
+            shotCounter = timeBetweenShots;
+
+            Instantiate(projectile, firePoint.position, firePoint.rotation);
+        }
+    }
+}
---
+++ TowerDefense/Assets/Scripts/Towers/Tower.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public float range = 3f;
+
+    public LayerMask whatIsEnemy;
+
+    private Collider[] colliderInRange;
+    public List<EnemyController> enemiesInRange = new List<EnemyController>();
+
+    private float checkCounter;
+    public float checkTime = .2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkCounter = checkTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkCounter = Time.deltaTime;
+        if(checkCounter <= 0)
+        {
+            checkCounter = checkTime;
+            colliderInRange = Physics.OverlapSphere(transform.position, range, whatIsEnemy);
+
+            enemiesInRange.Clear();
+            foreach(Collider col in colliderInRange)
+            {
+                enemiesInRange.Add(col.GetComponent<EnemyController>());
+            }
+        }
+
+    }
+}
